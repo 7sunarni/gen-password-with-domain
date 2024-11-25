c301294baeeb83ebaada0a465404efbc67bdd25c
7sunarni <710720732@qq.com>
2024-11-25 10:17:11 +0800

    fix(server): tls listen

--- main.go
+++ main.go
@@ -12,11 +12,15 @@ func main() {
 	workDir = filepath.Dir(os.Args[0])
 	port := os.Getenv("HTTP_PORT")
 	if port == "" {
-		port = "38080"
+		port = "80"
 	}
 
-	if err := http.ListenAndServeTLS("0.0.0.0:443", "/etc/letsencrypt/live/7sunarni.reborn.tk/fullchain.pem", "/etc/letsencrypt/live/7sunarni.reborn.tk/privkey.pem", NewMixedHandler()); err != nil {
+	key := os.Getenv("TLS_CERT")
+	cert := os.Getenv("TLS_KEY")
+	_, err := os.Stat(key)
+	if err != nil {
 		http.ListenAndServe("0.0.0.0:"+port, NewMixedHandler())
-
+	} else {
+		http.ListenAndServeTLS("0.0.0.0:443", key, cert, NewMixedHandler())
 	}
 }
